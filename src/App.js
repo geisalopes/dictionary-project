25ce2a2f4b284ead44094d81166002b93f14061e
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-01-08 13:54:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./dictionary-logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <h1>Dictionary App</h1>
+      </header>
+    </div>
+  );
+}
