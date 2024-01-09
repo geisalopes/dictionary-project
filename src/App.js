4ba73051d43d445c126cb36adc0608bb630ce13b
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-01-09 11:51:43 +0100

    Add search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from "./dictionary-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <h1>Dictionary App</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://geisalopes.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Geisa Lopes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/geisalopes/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useSate, useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
