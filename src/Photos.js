790bd3e52633ca033f9d0635e3ca9357a3f60c3a
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-01-15 15:33:22 +0100

    Fix bugs

--- src/Photos.js
+++ src/Photos.js
@@ -10,12 +10,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt=""
-                    rel="noreferrer"
-                  />
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
