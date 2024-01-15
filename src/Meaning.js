ba51271d6f72bd07adadfcf66c3f0af94ae247bc
geisalopes <72300375+geisalopes@users.noreply.github.com>
2024-01-15 16:59:37 +0100

    Style the font title

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,11 +10,12 @@ export default function Meaning(props) {
         <div>
           <strong>Definition: </strong>
           {props.meaning.definition}
-          <br />
-          <strong>Example: </strong>
-          <em> {props.meaning.example}</em>
-          <br />
-          {/* <strong>Synonyms: </strong> */}
+          {props.meaning.example && (
+            <div>
+              <strong>Example: </strong>
+              <em> {props.meaning.example}</em>
+            </div>
+          )}
           <Synonyms synonyms={props.meaning.synonyms} />
         </div>
       </section>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,8 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
-        <strong>Synonyms: </strong>
+        <strong>Synonyms </strong>
+        <br />
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
